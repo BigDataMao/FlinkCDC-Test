ae6fff305cf7838212c073b7480a102776020d2f
flinkmao <mxw712@gmail.com>
2024-06-11 16:51:00 +0800

    feature: 初步完成kafka-flink-doris

--- src/main/java/cn/chatdoge/flink117/source/KafkaSourceExample.java
+++ src/main/java/cn/chatdoge/flink117/source/KafkaSourceExample.java
@@ -1,10 +1,23 @@
 package cn.chatdoge.flink117.source;
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.doris.flink.cfg.DorisExecutionOptions;
+import org.apache.doris.flink.cfg.DorisOptions;
+import org.apache.doris.flink.sink.DorisSink;
+import org.apache.doris.flink.sink.writer.serializer.RowDataSerializer;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.DataType;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+import java.util.UUID;
 
 /**
  * @Description: 接Kafka数据源
@@ -22,13 +35,43 @@ public class KafkaSourceExample {
         prop.setProperty("group.id", "flink");  // 消费者组
 
         // 创建Kafka数据源
-        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(
-                "flume-kafka-flink",
-                new SimpleStringSchema(),
-                prop);
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                .setProperties(prop)
+                .setTopics("flume-kafka-flink")
+                .build();
 
         // 添加数据源
-        env.addSource(kafkaConsumer).print();
+        DataStream<RowData> dataStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafka source")
+                .map((MapFunction<String, RowData>) value -> {
+                    String uuid = UUID.randomUUID().toString();
+                    String dt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    GenericRowData genericRowData = new GenericRowData(3);
+                    genericRowData.setField(0, uuid);
+                    genericRowData.setField(1, dt);
+                    genericRowData.setField(2, value);
+                    return genericRowData;
+                });
+
+        // 添加doris连接参数
+        DorisOptions dorisOptions = DorisOptions.builder()
+                .setFenodes("cdh-master:8030")
+                .setTableIdentifier("flink.test")
+                .setUsername("admin")
+                .setPassword("BJcgSGk4(icB1Sczb")
+                .build();
+
+        // 添加doris sink
+        String[] fieldNames = {"uuid","dt","word"};
+        DataType[] types = {DataTypes.VARCHAR(255), DataTypes.STRING(), DataTypes.STRING()};
+        DorisSink.Builder<RowData> dorisBuilder = DorisSink.builder();
+        dorisBuilder.setDorisOptions(dorisOptions)
+                .setDorisExecutionOptions(DorisExecutionOptions.builder().build())
+                .setSerializer(RowDataSerializer.builder()
+                        .setFieldNames(fieldNames)
+                        .setFieldType(types)
+                        .build());
+
+        dataStream.sinkTo(dorisBuilder.build());
 
         // 执行任务
         env.execute("kafka source");
