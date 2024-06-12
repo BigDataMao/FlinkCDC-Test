10c04d9d34edce77cb3b4edaeb7b4ca55ca8b871
flinkmao <mxw712@gmail.com>
2024-06-12 12:13:06 +0800

    feature: 已验证通过

--- src/main/java/cn/chatdoge/flink117/source/KafkaSourceExample.java
+++ src/main/java/cn/chatdoge/flink117/source/KafkaSourceExample.java
@@ -2,8 +2,10 @@ package cn.chatdoge.flink117.source;
 
 import org.apache.doris.flink.cfg.DorisExecutionOptions;
 import org.apache.doris.flink.cfg.DorisOptions;
+import org.apache.doris.flink.cfg.DorisReadOptions;
 import org.apache.doris.flink.sink.DorisSink;
 import org.apache.doris.flink.sink.writer.serializer.RowDataSerializer;
+import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
@@ -15,14 +17,15 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.data.StringData;
+import org.apache.flink.table.data.TimestampData;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.util.Collector;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Instant;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -35,6 +38,10 @@ public class KafkaSourceExample {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+        // enable checkpoint
+        env.enableCheckpointing(10000);
+        // using batch mode for bounded data
+        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
 
         // kafka连接参数
         Properties prop = new Properties();
@@ -51,39 +58,60 @@ public class KafkaSourceExample {
         // 添加数据源
         DataStream<RowData> dataStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafka source")
                 .map((MapFunction<String, RowData>) value -> {
+                    // 打印从kafka中读取的数据
+                    System.out.println("flink获得的value: " + value);
+
                     String uuid = UUID.randomUUID().toString();
-                    String dt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    TimestampData dt = TimestampData.fromInstant(Instant.now());
                     GenericRowData genericRowData = new GenericRowData(3);
-                    genericRowData.setField(0, uuid);
+                    genericRowData.setField(0, StringData.fromString(uuid));
                     genericRowData.setField(1, dt);
-                    genericRowData.setField(2, value);
+                    genericRowData.setField(2, StringData.fromString(value));
+
+                    // 打印构造的RowData
+                    System.out.println("flink重组的RowData: " + genericRowData);
+
                     return genericRowData;
                 });
 
         // 添加doris连接参数
         DorisOptions dorisOptions = DorisOptions.builder()
-                .setFenodes("cdh-master:8030")
+                .setFenodes("cdh-master:8036")
                 .setTableIdentifier("flink.test")
                 .setUsername("admin")
                 .setPassword("BJcgSGk4(icB1Sczb")
                 .build();
 
+        // 添加doris执行参数
+        Properties properties = new Properties();
+        properties.setProperty("format", "json");
+        properties.setProperty("read_json_by_line", "true");
+        DorisExecutionOptions.Builder  executionBuilder = DorisExecutionOptions.builder();
+        executionBuilder.setLabelPrefix("label-doris")
+                .setDeletable(false)
+                .setStreamLoadProp(properties);
+
         // 添加doris sink
-        String[] fieldNames = {"uuid","dt","word"};
+        String[] fieldNames = {"uuid", "dt", "word"};
         DataType[] types = {
                 DataTypes.VARCHAR(255),
-                DataTypes.TIMESTAMP(),
+                DataTypes.TIMESTAMP(3),
                 DataTypes.STRING()
         };
+
         DorisSink.Builder<RowData> dorisBuilder = DorisSink.builder();
-        dorisBuilder.setDorisOptions(dorisOptions)
-                .setDorisExecutionOptions(DorisExecutionOptions.builder().build())
+        dorisBuilder.setDorisReadOptions(DorisReadOptions.builder().build())
+                .setDorisExecutionOptions(executionBuilder.build())
                 .setSerializer(RowDataSerializer.builder()
                         .setFieldNames(fieldNames)
+                        .setType("json")
                         .setFieldType(types)
-                        .build());
+                        .build())
+                .setDorisOptions(dorisOptions);
 
-        dataStream.sinkTo(dorisBuilder.build());
+        dataStream.sinkTo(dorisBuilder.build())
+                .name("doris sink")
+                .setParallelism(1);
 
         // 执行任务
         env.execute("kafka source");
@@ -92,7 +120,11 @@ public class KafkaSourceExample {
     private static class CustomKafkaDeserializationSchema implements KafkaRecordDeserializationSchema<String> {
         @Override
         public void deserialize(ConsumerRecord<byte[], byte[]> consumerRecord, Collector<String> collector) throws IOException {
-            collector.collect(new String(consumerRecord.value(), StandardCharsets.UTF_8));
+            String value = new String(consumerRecord.value(), StandardCharsets.UTF_8);
+            collector.collect(value);
+
+            // 打印从kafka中读取的数据
+            System.out.println("kafka反序列化器获得的value: " + value);
         }
 
         @Override
@@ -100,4 +132,5 @@ public class KafkaSourceExample {
             return Types.STRING;
         }
     }
+
 }
