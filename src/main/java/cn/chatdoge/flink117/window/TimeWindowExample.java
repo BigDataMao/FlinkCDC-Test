2b6d850e902f12268f5f990e3e1a237a81874503
毛新伟 <mxw712@gmail.com>
2024-07-31 22:41:54 +0800

    feature: 窗口和watermark稳定测试运行,写了注释,需要两个一起运行

--- src/main/java/cn/chatdoge/flink117/window/TimeWindowExample.java
+++ src/main/java/cn/chatdoge/flink117/window/TimeWindowExample.java
@@ -3,6 +3,13 @@ package cn.chatdoge.flink117.window;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+/**
+ * @author Samuel Mau
+ * @description 用来测试滚动时间窗口,结合watermark
+ * 运行的时候需要跟GenerateOutOfOrderDataStream一起运行
+ * 这样才能实时观察每条数据在什么时候触发窗口计算
+ */
+
 public class TimeWindowExample {
     public static void main(String[] args) throws Exception {
         // 创建表环境
@@ -26,6 +33,16 @@ public class TimeWindowExample {
                     'scan.startup.mode' = 'earliest-offset'
                 )""");
 
+        // 创建用于打印的表 (临时表)
+        tableEnv.executeSql("""
+                CREATE TABLE printTable (
+                  id INT,
+                  name STRING,
+                  event_time TIMESTAMP(3)
+                ) WITH (
+                  'connector' = 'print'
+                )""");
+
         // 创建滚动时间窗口,每5秒统计一次cnt值,connector为print
         tableEnv.executeSql("""
                 CREATE TABLE timeWindowTable (
@@ -35,6 +52,13 @@ public class TimeWindowExample {
                 ) WITH (
                   'connector' = 'print'
                 )""");
+
+        // 将每条数据插入到临时打印表中
+        tableEnv.executeSql("""
+                INSERT INTO printTable
+                SELECT id, name, event_time
+                FROM kafkaSource
+                """);
 
         // 执行窗口统计
         tableEnv.executeSql("""
